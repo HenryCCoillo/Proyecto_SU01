30637ec04340c195df1aaf56c6e607c3468228d5
KevinSucapuca <ksucapuca007@gmail.com>
2022-11-01 14:20:36 -0500

    Modificando la función Leer_Archivos

--- Tarea 1/main.py
+++ Tarea 1/main.py
@@ -17,14 +17,15 @@ class Libro:
 
     def leer_archivo(self):
         libro = input("Escriba el archivo : ")
-        if libro:
-            if path.exists(libro):            
-                with open(libro,encoding="utf-8") as archivo:
-                    print(archivo.read())
-            else:
-                print("El archivo no existe")
-        else:
-            print("Escriba el archivo")
+        while libro not in ("Libros"):
+            libro = input("Escriba el archivo nuevamente: ")
+        if libro == "Libros":
+            with open('D:\BootCamp\\'+libro+'.csv',encoding='utf-8-sig') as f:
+                archivo = csv.reader(f)
+                datos =[]
+                for i in archivo:
+                    datos.append(i)
+                print(datos)
 
 
     def listar_libros(self):
@@ -42,6 +43,71 @@ class Libro:
                 print(f"\n-El Libro llamano {nombre_libro_titulo}, su genero es {nombre_libro_genero}, su codigo de isbn es {nombre_libro_isbn}, la editorial es {nombre_libro_editorial} y el autor es {nombre_libro_autor}\n")
 
 
+if __name__ == '__main__':
+    try:
+        while True:
+            print("""
+Opción 1: Leer archivo de disco duro (.txt o csv) que cargue 3 libros.
+Opción 2: Listar libros.
+Opción 3: Agregar libro.
+Opción 4: Eliminar libro.
+Opción 5: Buscar libro por ISBN o por título. Se debe sugerir las opciones y listar el resultado.
+Opción 6: Ordenar libros por título.
+Opción 7: Buscar libros por autor, editorial o género. Se deben sugerir las opciones y listar los resultados.
+Opción 8: Buscar libros por número de autores. Se debe ingresar un número por ejemplo 2 (hace referencia a dos autores) y se deben listar todos los libros 
+que contengan 2 autores.
+Opción 9: Editar o actualizar datos de un libro (título, género, ISBN, editorial y autores).
+Opción 10: Guardar libros en archivo de disco duro (.txt o csv).
+Opción 11: Salir del Programa.
+            """)
+
+            while True:
+                try:
+                    opcion = int(input("Digite la Opcion: "))
+                except ValueError:
+                    print("Debes Escribir un numero Valido")
+                    continue
+                
+                if opcion:
+                    break
+
+            libro = Libro()
+            if opcion == 1:
+                libro.leer_archivo()
+            elif opcion ==2:
+                pass
+
+            elif opcion ==3:
+                pass
+
+            elif opcion ==4:
+                pass
+
+            elif opcion ==5:
+                pass
+
+            elif opcion ==6:
+                pass
+
+            elif opcion ==7:
+                pass
+
+            elif opcion ==8:
+                pass
+
+            elif opcion ==9:
+                pass
+
+            elif opcion ==10:
+                pass
+
+            elif opcion == 11:
+                del libro
+                break
+
+    except KeyboardInterrupt:
+        print("Saliendo...")
+        exit()
 
 
 
