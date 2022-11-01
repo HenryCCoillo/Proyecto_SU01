3e942dc7d301917674dccbd0483bdf19fa934300
HenryCCoillo <henryccoillo11@gmail.com>
2022-11-01 00:23:40 -0500

    Opciones de datos

--- Tarea 1/main.py
+++ Tarea 1/main.py
@@ -45,3 +45,60 @@ class Libro:
 
 
 
+if __name__ == '__main__':
+    try:
+        while True:
+            print("""
+Opción 1: Leer archivo de disco duro (.txt o csv) que cargue 3 libros.
+Opción 2: Listar libros.
+Opción 3: Agregar libro.
+Opción 4: Eliminar libro.
+Opción 5: Buscar libro por ISBN o por título. Se debe sugerir las opciones y listar el resultado.
+Opción 6: Ordenar libros por título.
+Opción 7: Buscar libros por autor, editorial o género. Se deben sugerir las opciones y listar los resultados.
+Opción 8: Buscar libros por número de autores. Se debe ingresar un número por ejemplo 2 (hace referencia a dos autores) y se deben listar todos los libros 
+que contengan 2 autores.
+Opción 9: Editar o actualizar datos de un libro (título, género, ISBN, editorial y autores).
+Opción 10: Guardar libros en archivo de disco duro (.txt o csv).
+Opción 11: Salir del Programa.
+            """)
+            opcion = int(input("Digite la Opcion: "))
+
+            libro = Libro()
+            if opcion == 1:
+                libro.leer_archivo()
+            elif opcion ==2:
+                pass
+
+            elif opcion ==3:
+                pass
+
+            elif opcion ==4:
+                pass
+
+            elif opcion ==5:
+                pass
+
+            elif opcion ==6:
+                pass
+
+            elif opcion ==7:
+                pass
+
+            elif opcion ==8:
+                pass
+
+            elif opcion ==9:
+                pass
+
+            elif opcion ==10:
+                pass
+
+            elif opcion == 11:
+                del libro
+                break
+
+    except KeyboardInterrupt:
+        print("Saliendo...")
+        exit()
+
