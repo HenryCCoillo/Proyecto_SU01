ee43dec3f3c9f17e3e9abe08f6f5a09991da9915
HenryCCoillo <henryccoillo11@gmail.com>
2022-10-31 23:52:58 -0500

    Inicio

---
+++ Tarea 1/main.py
@@ -0,0 +1,47 @@
+from ast import While
+from os import path
+import json
+
+class Libro:
+    def __init__(self,id:str = None ,titulo:str = None,genero:str= None,isbn= None,editorial= None,autor= None) -> None:
+        self.__id = id
+        self.__titulo = titulo
+        self.__genero = genero
+        self.__isbn = isbn
+        self.__editorial = editorial
+        self.__autor = autor
+
+    def __del__(self):
+        return None
+
+    def leer_archivo(self):
+        libro = input("Escriba el archivo : ")
+        if libro:
+            if path.exists(libro):            
+                with open(libro,encoding="utf-8") as archivo:
+                    print(archivo.read())
+            else:
+                print("El archivo no existe")
+        else:
+            print("Escriba el archivo")
+
+
+    def listar_libros(self):
+        with open("prueba.json",encoding="utf-8") as archivo:
+            datos_libros = json.load(archivo)
+
+            for i in datos_libros:
+                diccionario_libro = datos_libros[i]
+                nombre_libro_id =diccionario_libro["id"]
+                nombre_libro_titulo = diccionario_libro["titulo"]
+                nombre_libro_genero =diccionario_libro["genero"] 
+                nombre_libro_isbn =diccionario_libro["isbn"] 
+                nombre_libro_editorial =diccionario_libro["editorial"] 
+                nombre_libro_autor =diccionario_libro["autor"] 
+                print(f"\n-El Libro llamano {nombre_libro_titulo}, su genero es {nombre_libro_genero}, su codigo de isbn es {nombre_libro_isbn}, la editorial es {nombre_libro_editorial} y el autor es {nombre_libro_autor}\n")
+
+
+
+
+
+
