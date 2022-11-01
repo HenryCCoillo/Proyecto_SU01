d310e0d96db19660913a9509cc1a9caa5027c4db
KevinSucapuca <ksucapuca007@gmail.com>
2022-11-01 16:55:36 -0500

    Agregando la función agregar_libros

--- Tarea 1/main.py
+++ Tarea 1/main.py
@@ -1,5 +1,7 @@
 from ast import While
 from os import path
+import json
+from csv import DictWriter
 import csv
 import pandas as pd
 
@@ -20,7 +22,7 @@ class Libro:
         while libro not in ("Libros"):
             libro = input("Escriba el archivo nuevamente: ")
         if libro == "Libros":
-            with open('D:\BootCamp\\'+libro+'.csv',encoding='utf-8-sig') as f:
+            with open('D:\\BootCamp\\Proyecto_Silabuz\\Proyecto_SU01\\Tarea 1\\'+libro+'.csv',encoding='utf-8-sig') as f:
                 archivo = csv.reader(f)
                 datos =[]
                 for i in archivo:
@@ -29,18 +31,41 @@ class Libro:
 
 
     def listar_libros(self):
-        data=pd.read_csv("D:\BootCamp\\Libros.csv")
-        data
+        data=pd.read_csv("D:\\BootCamp\\Proyecto_Silabuz\\Proyecto_SU01\\Tarea 1\\Libros.csv")
+        print(data)
+        
+    
+    def agregar_libros(self):
+        
+        codigo=input("ingrese ID: ")
+        titulo=input("Ingrese título: ")
+        genero=input("Ingrese genero: ")
+        isbn=input("Ingrese ISBN: ")
+        editorial=input("Ingrese editorial: ")
+        autor=input("Ingrese autor: ")
+        
+        headersCSV = ['id','titulo','genero','isbn','editorial','autor']      
+
+        dict={'id':codigo,'titulo':titulo,'genero':genero,'isbn':isbn,'editorial':editorial,'autor':autor}
+        with open('D:\\BootCamp\\Proyecto_Silabuz\\Proyecto_SU01\\Tarea 1\\Libros.csv', 'a', newline='',encoding='utf-8-sig') as f_object:
+    
+            dictwriter_object = DictWriter(f_object, fieldnames=headersCSV)
+    
+            dictwriter_object.writerow(dict)
+   
+            f_object.close()
+
+
 
 if __name__ == '__main__':
     try:
         while True:
             print("""
-Opción 1: Leer archivo de disco duro (csv).
+Opción 1: Leer archivo de disco duro (.txt o csv) que cargue 3 libros.
 Opción 2: Listar libros.
 Opción 3: Agregar libro.
 Opción 4: Eliminar libro.
-Opción 5: Buscar libro por título()
+Opción 5: Buscar libro por ISBN o por título. Se debe sugerir las opciones y listar el resultado.
 Opción 6: Ordenar libros por título.
 Opción 7: Buscar libros por autor, editorial o género. Se deben sugerir las opciones y listar los resultados.
 Opción 8: Buscar libros por número de autores. Se debe ingresar un número por ejemplo 2 (hace referencia a dos autores) y se deben listar todos los libros 
@@ -63,11 +88,11 @@ Opción 11: Salir del Programa.
             libro = Libro()
             if opcion == 1:
                 libro.leer_archivo()
-            elif opcion ==2:
+            elif opcion == 2:
                 libro.listar_libros()
 
-            elif opcion ==3:
-                pass
+            elif opcion == 3:
+                libro.agregar_libros()
 
             elif opcion ==4:
                 pass
@@ -83,6 +108,7 @@ Opción 11: Salir del Programa.
 
             elif opcion ==8:
                 pass
+                
 
             elif opcion ==9:
                 pass
@@ -97,7 +123,6 @@ Opción 11: Salir del Programa.
     except KeyboardInterrupt:
         print("Saliendo...")
         exit()
-
 
 
 
