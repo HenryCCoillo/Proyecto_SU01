c67e55242c1cee04b213e80b0d107171e8c6b2a7
KevinSucapuca <ksucapuca007@gmail.com>
2022-11-02 23:37:28 -0500

    Add function eliminar_libro

--- Tarea 1/main.py
+++ Tarea 1/main.py
@@ -32,7 +32,9 @@ class Libro:
 
     def listar_libros(self):
         data=pd.read_csv("D:\\BootCamp\\Proyecto_Silabuz\\Proyecto_SU01\\Tarea 1\\Libros.csv")
-        print(data)
+        data2=data[['titulo','genero','isbn','editorial','autor']]
+        data3=data2.to_string(index=False)
+        print(data3)
         
     
     def agregar_libros(self):
@@ -52,6 +54,7 @@ class Libro:
             dictwriter_object = DictWriter(f_object, fieldnames=headersCSV)
     
             dictwriter_object.writerow(dict)
+            
    
             f_object.close()
 
@@ -65,7 +68,7 @@ Opción 1: Leer archivo de disco duro (.txt o csv) que cargue 3 libros.
 Opción 2: Listar libros.
 Opción 3: Agregar libro.
 Opción 4: Eliminar libro.
-Opción 5: Buscar libro por ISBN o por título. Se debe sugerir las opciones y listar el resultado.
+Opción 5: Buscar libro por título. Se debe sugerir las opciones y listar el resultado.
 Opción 6: Ordenar libros por título.
 Opción 7: Buscar libros por autor, editorial o género. Se deben sugerir las opciones y listar los resultados.
 Opción 8: Buscar libros por número de autores. Se debe ingresar un número por ejemplo 2 (hace referencia a dos autores) y se deben listar todos los libros 
